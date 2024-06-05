c33c58848a98dc3b67701a2166caa549b0621550
Paulo <info@monis-consulting.com>
2024-06-05 14:21:06 +0100

    now json-server runs before the application

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), eslint()],
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "http://localhost:9000",
     },
   },
 });
